ed5fe8da5ffdcf0b1c753779ab435eef155e7cb8
Angarsk8 <andresa.garciah621@gmail.com>
2015-10-14 21:49:23 -0500

    Add a bash script to automate the terminal configuration

---
+++ terminal_config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a file_names=(.bash_profile .bash_prompt .aliases)
+url=https://raw.githubusercontent.com/Angarsk8/solarized-dark-iterm/master
+
+if [[ -f $(which curls) ]]; then
+	for file in "${file_names[@]}" 
+	do
+		full_path=$url/$file
+		echo "Downloading $file"
+		curl $full_path >> $file
+	done
+	echo "The configuration process has finished, quit iterm and re-open it and have fun with you new iterm look!!"
+	exit 0
+else
+	echo "The configuration failed because the curl utility is not installed on this machine" 
+	exit 1
+fi
